f016dda4634d3a6776f863a2e8feaa110a48798d
Ринат <qwe>
2022-09-29 19:34:16 +0500

    1.0.0

---
+++ laravel9/laravel9/routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/login', [UserController::class,'login'])->name('login');
+Route::post('/login', [UserController::class,'loginPost']);
+
+Route::get('/register',[UserController::class, 'register'])->name('register');
+Route::post('/register',[UserController::class, 'registerPost']);
+
+Route::middleware('auth')->group(function (){
+
+    Route::middleware('role:user,admin')->group(function (){
+
+        Route::middleware('role:admin')->group(function (){
+            Route::prefix('/admin')->group(function (){
+                Route::resource('/product', ProductController::class);
+            });
+        });
+    });
+
+    Route::get('/logout', [UserController::class,'logout'])->name('logout');
+});
